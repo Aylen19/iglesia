2ad4406b4e420601ce3ad2e2651fbc6836908359
Fernando Garcia <jampasnando@gmail.com>
2022-02-10 20:04:53 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sistembo.sandy',
+  appName: 'sandy',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
